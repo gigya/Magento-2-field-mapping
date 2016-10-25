68b9b8073996984cdcc70dbfc8d2c4468c9e3c3b
luciodiri <guy.avivi@gigya-inc.com>
2016-10-25 10:02:11 +0300

    init

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+//ComponentRegistrar::register(ComponentRegistrar::MODULE, '<Gigya_GigyaIM>', __DIR__);
+
+ \Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Gigya_FieldMapping',
+    __DIR__
+);
