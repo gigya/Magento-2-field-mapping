5bb7613d482848bd2a99aae4ab113b0cac237ef9
luciodiri <guy.avivi@gigya-inc.com>
2016-10-25 14:53:25 +0300

    establish observer

--- Observer/MapFieldsObserver.php
+++ Observer/MapFieldsObserver.php
@@ -1,24 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: guy.av
- * Date: 25/10/2016
- * Time: 12:52
+ * Observer for enriching user data before
  */
 
 namespace Gigya\Gigya_FieldMapping\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
 
 class MapFieldsObserver implements ObserverInterface
 {
     public function __construct()
     {
         //Observer initialization code...
-        //You can use dependency injection to get any class this observer may need.
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $myEventData = $observer->getData('gigya_user');
+        $gigya_user = $observer->getData('gigya_user');
+        $customer = $observer->getData('customer');
         //Additional observer execution code...
     }
 }
