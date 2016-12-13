b873233191cefa677d0f9b7032dd48650c8352ef
luciodiri <guy.avivi@gigya-inc.com>
2016-12-13 11:19:00 +0200

    add cache

---
+++ Model/Cache/Type.php
@@ -0,0 +1,26 @@
+<?php
+namespace Gigya\Gigya_FieldMapping\Model\Cache;
+
+/**
+ * System / Cache Management / Cache type "Custom Cache Tag"
+ */
+class Type extends \Magento\Framework\Cache\Frontend\Decorator\TagScope
+{
+    /**
+     * Cache type code unique among all cache types
+     */
+    const TYPE_IDENTIFIER = 'gigya_cache_tag';
+
+    /**
+     * Cache tag used to distinguish the cache type from all other cache
+     */
+    const CACHE_TAG = 'GIGYA_CACHE_TAG';
+
+    /**
+     * @param \Magento\Framework\App\Cache\Type\FrontendPool $cacheFrontendPool
+     */
+    public function __construct(\Magento\Framework\App\Cache\Type\FrontendPool $cacheFrontendPool)
+    {
+        parent::__construct($cacheFrontendPool->get(self::TYPE_IDENTIFIER), self::CACHE_TAG);
+    }
+}
